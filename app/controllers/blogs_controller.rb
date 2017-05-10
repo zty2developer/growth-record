59cd67ed6daaee9110740ba5858b04d921a144c0
Talon.Zhou <ztyemail@126.com>
2017-05-10 19:11:56 +0800

    修改了blog页中的每页展示post的条数

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,12 +1,12 @@
 class BlogsController < ApplicationController
 	def index
 		@categories = Category.all
-		@posts = Post.all.page(params[:page])
+		@posts = Post.all.page(params[:page]).per(6)
 	end
 
 	def category
 		category_id = params[:category_id]
-		@posts = Post.all.page(params[:page])
-		@posts = Post.where(category_id: category_id).page(params[:page]) if category_id
+		@posts = Post.all.page(params[:page]).per(6)
+		@posts = Post.where(category_id: category_id).page(params[:page]).per(6) if category_id
 	end
 end
